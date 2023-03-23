2b32557b736ea24336e6ce1ebde23a34168f6aca
DarkSammie <samuelabolarinwa977@gmail.com>
2023-03-22 20:15:06 -0700

    New commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,30 +3,33 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
+ *
  * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ *         0 and prints 0 if n is zero
+ *         -1 and prints - if n is less than zero
+ *         0 and prints nothing if n is not a number
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar(48);
-                return (0);
-        }
-        else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
-        else
-        {
-                /* Handle unexpected input */
-                return (0);
-        }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		/* Handle unexpected input */
+		return (0);
+	}
 }
+
